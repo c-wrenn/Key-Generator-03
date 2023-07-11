590dd80bfe6afafc3a69246c6f25959dcc1318df
c-wrenn <crislynwren@yahoo.com>
2023-07-11 01:33:58 -0500

    * Added variables for password composition * Added random character generator * Gave user options of which characters they would like to include

--- script.js
+++ script.js
@@ -1,5 +1,44 @@
 // Assignment code here
+var lowerCase = 'abcdefghijklmnopqrstuvwxyz'
+var upperCase  =  lowerCase.toUpperCase()
+var numbers ="0123456789"
+var special = "!@#$%^&*()"
+var candidates =''
+var password = ''
 
+
+function generatePassword() {
+  var length = prompt("How long do you want your password? ")
+  var parsedLength = parseInt(length)
+  console.log(length)
+  var lowercaseAnswer = confirm("Would you like to include lowercase characters?")
+  var upperCaseAnswer = confirm("Would you like to include upper case characters?")
+  var numbersAnswer = confirm("Would you like to include numbers?")
+  var specialAnswer = confirm("Would you like to include special characters?")
+  console.log(lowercaseAnswer)
+  if (lowercaseAnswer === true) {
+    candidates += lowerCase
+  }
+  if (upperCaseAnswer === true) {
+    candidates += upperCase
+  }
+  if (numbersAnswer   === true) {
+    candidates += numbers
+  }
+  if (specialAnswer   === true) { 
+    candidates += special
+  }
+
+  
+    for (var i = 0; i < parsedLength; i++) {
+      var k= candidates.length
+console.log(k)
+      var newCharacter = candidates[parseInt(Math.random() * k)] //0.22225569 *26 = 4.236589 = 4
+      password += newCharacter
+    }
+  
+  return password
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
